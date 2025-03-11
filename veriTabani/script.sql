3c11abb8af5bece8e845c538c9f1aa06aec0b495
Mustafa Al-Hussayan <136298486+1mmh3p@users.noreply.github.com>
2025-03-12 02:59:54 +0300

    Add files via upload

---
+++ veriTabani/script.sql
@@ -0,0 +1,128 @@
+CREATE DATABASE [HastaneProje]
+GO
+USE [HastaneProje]
+GO
+/****** Object:  Table [dbo].[Tbl_Branslar]    Script Date: 25/07/2025 14:38:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Tbl_Branslar](
+	[Bransid] [tinyint] IDENTITY(1,1) NOT NULL,
+	[BransAd] [varchar](30) NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Tbl_Doktorlar]    Script Date: 25/07/2025 14:38:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Tbl_Doktorlar](
+	[Doktorid] [tinyint] IDENTITY(1,1) NOT NULL,
+	[DoktorAd] [varchar](10) NULL,
+	[DoktorSoyad] [varchar](10) NULL,
+	[DoktorBrans] [varchar](30) NULL,
+	[DoktorTC] [char](11) NULL,
+	[DoktorSifre] [varchar](10) NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Tbl_Duyurular]    Script Date: 25/07/2025 14:38:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Tbl_Duyurular](
+	[Duyuruid] [smallint] IDENTITY(1,1) NOT NULL,
+	[Duyuru] [varchar](200) NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Tbl_Hastalar]    Script Date: 25/07/2025 14:38:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Tbl_Hastalar](
+	[Hastaid] [smallint] IDENTITY(1,1) NOT NULL,
+	[HastaAd] [varchar](10) NULL,
+	[HastaSoyad] [varchar](10) NULL,
+	[HastaTC] [char](11) NULL,
+	[HastaTelefon] [varchar](15) NULL,
+	[HastaSifre] [varchar](10) NULL,
+	[HastaCinsiyet] [varchar](5) NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Tbl_Randevular]    Script Date: 25/07/2025 14:38:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Tbl_Randevular](
+	[Randevuid] [int] IDENTITY(1,1) NOT NULL,
+	[RandevuTarih] [varchar](10) NULL,
+	[RandevuSaat] [varchar](5) NULL,
+	[RandevuBrans] [varchar](30) NULL,
+	[RandevuDoktor] [varchar](20) NULL,
+	[RandevuDurum] [bit] NULL,
+	[HastaTC] [char](11) NULL,
+	[HastaSikayet] [varchar](250) NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Tbl_Sekreter]    Script Date: 25/07/2025 14:38:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Tbl_Sekreter](
+	[Sekreterid] [tinyint] IDENTITY(1,1) NOT NULL,
+	[SekreterAdSoyad] [varchar](20) NULL,
+	[SekreterTC] [char](11) NULL,
+	[SekreterSifre] [varchar](10) NULL
+) ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[Tbl_Branslar] ON 
+
+INSERT [dbo].[Tbl_Branslar] ([Bransid], [BransAd]) VALUES (1, N'Dahiliye')
+INSERT [dbo].[Tbl_Branslar] ([Bransid], [BransAd]) VALUES (2, N'Göz')
+INSERT [dbo].[Tbl_Branslar] ([Bransid], [BransAd]) VALUES (3, N'Kulak Burun Boğaz')
+INSERT [dbo].[Tbl_Branslar] ([Bransid], [BransAd]) VALUES (5, N'Cilt Hastalıkları')
+SET IDENTITY_INSERT [dbo].[Tbl_Branslar] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Tbl_Doktorlar] ON 
+
+INSERT [dbo].[Tbl_Doktorlar] ([Doktorid], [DoktorAd], [DoktorSoyad], [DoktorBrans], [DoktorTC], [DoktorSifre]) VALUES (3, N'Mehmet', N'Yücedağ', N'Dahiliye', N'33333333333', N'33333')
+INSERT [dbo].[Tbl_Doktorlar] ([Doktorid], [DoktorAd], [DoktorSoyad], [DoktorBrans], [DoktorTC], [DoktorSifre]) VALUES (4, N'Akın', N'Yıldız', N'Göz', N'11111111111', N'1111')
+INSERT [dbo].[Tbl_Doktorlar] ([Doktorid], [DoktorAd], [DoktorSoyad], [DoktorBrans], [DoktorTC], [DoktorSifre]) VALUES (6, N'Şerif', N'Yılmaz', N'Göz', N'88888888888', N'8888')
+SET IDENTITY_INSERT [dbo].[Tbl_Doktorlar] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Tbl_Duyurular] ON 
+
+INSERT [dbo].[Tbl_Duyurular] ([Duyuruid], [Duyuru]) VALUES (1, N'Saat 21:00 ''de toplantı olacaktır.')
+SET IDENTITY_INSERT [dbo].[Tbl_Duyurular] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Tbl_Hastalar] ON 
+
+INSERT [dbo].[Tbl_Hastalar] ([Hastaid], [HastaAd], [HastaSoyad], [HastaTC], [HastaTelefon], [HastaSifre], [HastaCinsiyet]) VALUES (1, N'Buse', N'Kara', N'54615151656', N'(546) 546-4644', N'12345', N'Kadın')
+INSERT [dbo].[Tbl_Hastalar] ([Hastaid], [HastaAd], [HastaSoyad], [HastaTC], [HastaTelefon], [HastaSifre], [HastaCinsiyet]) VALUES (2, N'Doruk', N'Kara', N'11111111111', N'(111) 111-1111', N'1111', N'Erkek')
+SET IDENTITY_INSERT [dbo].[Tbl_Hastalar] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Tbl_Randevular] ON 
+
+INSERT [dbo].[Tbl_Randevular] ([Randevuid], [RandevuTarih], [RandevuSaat], [RandevuBrans], [RandevuDoktor], [RandevuDurum], [HastaTC], [HastaSikayet]) VALUES (1, N'24/07/2025', N'14:00', N'Dahiliye', N'Mehmet Yücedağ', 0, NULL, NULL)
+INSERT [dbo].[Tbl_Randevular] ([Randevuid], [RandevuTarih], [RandevuSaat], [RandevuBrans], [RandevuDoktor], [RandevuDurum], [HastaTC], [HastaSikayet]) VALUES (2, N'25/07/2025', N'12:00', N'Göz', N'Akın Yıldız', 1, N'11111111111', N'Gözüm ağrıyor.')
+INSERT [dbo].[Tbl_Randevular] ([Randevuid], [RandevuTarih], [RandevuSaat], [RandevuBrans], [RandevuDoktor], [RandevuDurum], [HastaTC], [HastaSikayet]) VALUES (3, N'26/07/2025', N'13:00', N'Dahiliye', N'Mehmet Yücedağ', 0, NULL, NULL)
+INSERT [dbo].[Tbl_Randevular] ([Randevuid], [RandevuTarih], [RandevuSaat], [RandevuBrans], [RandevuDoktor], [RandevuDurum], [HastaTC], [HastaSikayet]) VALUES (4, N'26/07/2025', N'14:00', N'Dahiliye', N'Mehmet Yücedağ', 0, NULL, NULL)
+INSERT [dbo].[Tbl_Randevular] ([Randevuid], [RandevuTarih], [RandevuSaat], [RandevuBrans], [RandevuDoktor], [RandevuDurum], [HastaTC], [HastaSikayet]) VALUES (7, N'26/07/2025', N'13:00', N'Göz', N'Akın Yıldız', 0, NULL, NULL)
+INSERT [dbo].[Tbl_Randevular] ([Randevuid], [RandevuTarih], [RandevuSaat], [RandevuBrans], [RandevuDoktor], [RandevuDurum], [HastaTC], [HastaSikayet]) VALUES (5, N'26/07/2025', N'15:00', N'Dahiliye', N'Mehmet Yücedağ', 0, NULL, NULL)
+INSERT [dbo].[Tbl_Randevular] ([Randevuid], [RandevuTarih], [RandevuSaat], [RandevuBrans], [RandevuDoktor], [RandevuDurum], [HastaTC], [HastaSikayet]) VALUES (6, N'26/07/2025', N'12:00', N'Göz', N'Akın Yıldız', 0, NULL, NULL)
+INSERT [dbo].[Tbl_Randevular] ([Randevuid], [RandevuTarih], [RandevuSaat], [RandevuBrans], [RandevuDoktor], [RandevuDurum], [HastaTC], [HastaSikayet]) VALUES (8, N'26/07/2025', N'14:00', N'Göz', N'Şerif Yılmaz', 0, NULL, NULL)
+INSERT [dbo].[Tbl_Randevular] ([Randevuid], [RandevuTarih], [RandevuSaat], [RandevuBrans], [RandevuDoktor], [RandevuDurum], [HastaTC], [HastaSikayet]) VALUES (9, N'26/07/2025', N'13:00', N'Göz', N'Şerif Yılmaz', 0, NULL, NULL)
+SET IDENTITY_INSERT [dbo].[Tbl_Randevular] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Tbl_Sekreter] ON 
+
+INSERT [dbo].[Tbl_Sekreter] ([Sekreterid], [SekreterAdSoyad], [SekreterTC], [SekreterSifre]) VALUES (1, N'Emel Öztürk', N'55555555555', N'5555')
+INSERT [dbo].[Tbl_Sekreter] ([Sekreterid], [SekreterAdSoyad], [SekreterTC], [SekreterSifre]) VALUES (2, N'Ali Güneş', N'66666666666', N'6666')
+SET IDENTITY_INSERT [dbo].[Tbl_Sekreter] OFF
+GO
+ALTER TABLE [dbo].[Tbl_Randevular] ADD  CONSTRAINT [DF_Tbl_Randevular_RandevuDurum]  DEFAULT ((0)) FOR [RandevuDurum]
+GO
